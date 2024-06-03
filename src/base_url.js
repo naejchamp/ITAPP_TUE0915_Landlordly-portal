1f5fdb7236ff1984eee8b0098ac1b6d99673ec08
naejchamp <naejchamp@tutanota.com>
2024-06-03 16:52:49 +0500

    asd

--- src/base_url.js
+++ src/base_url.js
@@ -1,2 +1,5 @@
-const base_url = "https://landlordy-be.vercel.app"
+const base_url = "https://landlordy-be-git-main-naejchamps-projects.vercel.app"
+
+// const base_url = "https://landlordy-be.vercel.app"
+
 export default base_url
