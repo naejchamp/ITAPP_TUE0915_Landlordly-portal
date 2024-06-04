84a505df6e282a5a355390c6706b30853bc8716d
naejchamp <naejchamp@tutanota.com>
2024-06-04 22:22:54 +0200

    Update header.js
    adding a description

--- src/components/main/landing/header.js
+++ src/components/main/landing/header.js
@@ -13,8 +13,7 @@ const Header = ()=>{
           Your Family
         </h1>
         <p className="animated fadeIn mb-4 pb-2">
-          Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no.
-          Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.
+          Discover your dream home sweet home with us. Explore our platform filled with cozy retreats, charming abodes, and enchanting spaces
         </p>
         <a href="" className="btn btn-primary py-3 px-5 me-3 animated fadeIn">
           Get Started
